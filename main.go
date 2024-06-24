9004d58916565e2b21fbda27e2006fc84b5a05cf
dai0115 <hateen0808@gmail.com>
2024-06-24 22:28:58 +0900

    update

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello, World!")
+        fmt.Fprintln(w, "Hello, wonderful world!")
     })
 
-    port := "8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
     fmt.Printf("Listening and serving HTTP on :%s\n", port)
     if err := http.ListenAndServe(":"+port, nil); err != nil {
         fmt.Printf("Failed to start server: %v\n", err)
